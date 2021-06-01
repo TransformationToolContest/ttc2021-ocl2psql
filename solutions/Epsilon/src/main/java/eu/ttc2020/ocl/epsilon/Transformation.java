245b6f2db0d05c4ff36461555125eb02b80ca04d
Antonio García-Domínguez <a.garcia-dominguez@aston.ac.uk>
2021-06-01 17:56:09 +0100

    Add Java program that transforms all queries

--- solutions/Epsilon/src/main/java/eu/ttc2020/ocl/epsilon/Transformation.java
+++ solutions/Epsilon/src/main/java/eu/ttc2020/ocl/epsilon/Transformation.java
@@ -1,15 +1,23 @@
 package eu.ttc2020.ocl.epsilon;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 import org.eclipse.epsilon.common.util.StringProperties;
 import org.eclipse.epsilon.emc.emf.EmfModel;
 import org.eclipse.epsilon.etl.EtlModule;
 import org.eclipse.epsilon.etl.launch.EtlRunConfiguration;
+import org.vgu.se.sql.parser.SQLParser;
+
+import ocl.dm.DmPackage;
+import ocl.exp.ExpPackage;
+import sql.SqlPackage;
 
 /**
  * Java wrapper for M2M transformation from OCL queries serialised in XMI using the
@@ -36,17 +44,15 @@ public class Transformation {
 	public void run() throws Exception {
 		StringProperties sourceProperties = new StringProperties();
 		sourceProperties.setProperty(EmfModel.PROPERTY_NAME, "OCL");
-		sourceProperties.setProperty(EmfModel.PROPERTY_FILE_BASED_METAMODEL_URI, resolveResource(OCL_MM_RESOURCE_PATH));
+		sourceProperties.setProperty(EmfModel.PROPERTY_METAMODEL_URI, DmPackage.eNS_URI + "," + ExpPackage.eNS_URI);
 		sourceProperties.setProperty(EmfModel.PROPERTY_MODEL_URI, getAbsoluteURIString(sourceModelFile));
-		sourceProperties.setProperty(EmfModel.PROPERTY_REUSE_UNMODIFIED_FILE_BASED_METAMODELS, true + "");
 		sourceProperties.setProperty(EmfModel.PROPERTY_READONLOAD, true + "");
 		sourceProperties.setProperty(EmfModel.PROPERTY_STOREONDISPOSAL, false + "");
 
 		StringProperties targetProperties = new StringProperties();
 		targetProperties.setProperty(EmfModel.PROPERTY_NAME, "SQL");
-		targetProperties.setProperty(EmfModel.PROPERTY_FILE_BASED_METAMODEL_URI, resolveResource(SQL_MM_RESOURCE_PATH));
+		targetProperties.setProperty(EmfModel.PROPERTY_METAMODEL_URI, SqlPackage.eNS_URI);
 		targetProperties.setProperty(EmfModel.PROPERTY_MODEL_URI, getAbsoluteURIString(targetModelFile));
-		targetProperties.setProperty(EmfModel.PROPERTY_REUSE_UNMODIFIED_FILE_BASED_METAMODELS, true + "");
 		targetProperties.setProperty(EmfModel.PROPERTY_READONLOAD, false + "");
 		targetProperties.setProperty(EmfModel.PROPERTY_STOREONDISPOSAL, true + "");
 
@@ -60,6 +66,7 @@ public class Transformation {
 			.build();
 
 		runConfig.run();
+		runConfig.dispose();
 	}
 
 	private String resolveResource(final String resourcePath) throws URISyntaxException {
@@ -83,9 +90,33 @@ public class Transformation {
 			System.exit(2);
 		}
 
-		for (File oclXMIFile : modelsFolder.listFiles(f -> f.getName().startsWith("Stage") && f.getName().endsWith(".xmi"))) {
-			File sqlFile = new File("ocl-" + oclXMIFile.getName());
-			new Transformation(oclXMIFile, sqlFile).run();
+		// Ensure all static metamodels are registered
+		DmPackage.eINSTANCE.getName();
+		ExpPackage.eINSTANCE.getName();
+		SqlPackage.eINSTANCE.getName();
+
+		File outputFolder = new File("outputs");
+		if (outputFolder.isDirectory()) {
+			for (File f : outputFolder.listFiles()) {
+				f.delete();
+			}
+		} else {
+			outputFolder.mkdirs();
+		}
+
+		File[] challengeFiles = modelsFolder.listFiles(f -> f.getName().startsWith("Stage") && f.getName().endsWith(".xmi"));
+		Arrays.sort(challengeFiles, (a, b) -> a.getName().compareTo(b.getName()));
+		for (File oclXMIFile : challengeFiles) {
+			final File sqlXMIFile = new File(outputFolder, "ocl-" + oclXMIFile.getName());
+			System.out.println(String.format("Transforming %s into %s", oclXMIFile.getPath(), sqlXMIFile.getPath()));
+			new Transformation(oclXMIFile, sqlXMIFile).run();
+
+			final String sqlStatement = SQLParser.outputEStatementAsString(sqlXMIFile);
+			final File sqlTextFile = new File(outputFolder, "ocl-" + oclXMIFile.getName().replaceAll("[.]xmi$", ".sql")); 
+			System.out.println(String.format("Transforming %s into %s", sqlXMIFile.getPath(), sqlTextFile.getPath()));
+			try (FileWriter fw = new FileWriter(sqlTextFile); PrintWriter pw = new PrintWriter(fw)) {
+				pw.println(sqlStatement);
+			}
 		}
 	}
 
