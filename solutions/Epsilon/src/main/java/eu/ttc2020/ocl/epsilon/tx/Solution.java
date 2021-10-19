5fa7882de831005864f9dd0cd9a22b03279830d7
Antonio García-Domínguez <a.garcia-dominguez@aston.ac.uk>
2021-10-19 17:17:15 +0100

    Include M2T in times, and use warmup iteration

--- solutions/Epsilon/src/main/java/eu/ttc2020/ocl/epsilon/tx/Solution.java
+++ solutions/Epsilon/src/main/java/eu/ttc2020/ocl/epsilon/tx/Solution.java
@@ -6,49 +6,50 @@ import org.eclipse.epsilon.emc.emf.EmfModel;
 
 import eu.ttc2020.ocl.epsilon.Configuration;
 import eu.ttc2020.ocl.epsilon.CorrectnessTest;
-import sql.SelectStatement;
 
 public class Solution {
 
-	// Transformation time from OCL expression model to SQL statement model
+	// Transformation time from OCL expression model to SQL statement model (first run, without warming up JVM)
+	private static final String METRIC_OCL2PSQL_TIME_WARMUP = "TransformTimeNanosWarmup";
+
+	// Transformation time from OCL expression model to SQL statement model (average of N runs afterwards)
 	private static final String METRIC_OCL2PSQL_TIME = "TransformTimeNanos";
+
+	// Number of runs to use for TransformTimeNanos
+	private static final int N_RUNS = 10;
+
 	// Execution time of SQL statements
 	private static final String METRIC_TEST_TIME = "TestTimeNanos";
+
 	// Database scenarios, from 1 to 7. More detail in docs/scenarios.
 	private static final String METRIC_SCENARIO_PREFIX = "Scenario%d";
 	private static final String METRIC_SCENARIO_ALL_PREFIX = "ScenarioID";
+
 	private static final int scenarios = 7;
 
 	public void run(Configuration c) {
-		//	Transformation time
-		long transformationTimeNano = 0;
+		String sqlStmString = null;
 
-		sql.SelectStatement sqlStmXMI = null;
-		EmfModel sqlModel = null;
+		/*
+		 * Reviewer 1 suggested that the results should not reflect the JVM startup, but the fact
+		 * is that we *do* have some startup costs, and throwing those away could impact the
+		 * validity of the results. As a compromise, we can show the results from a fresh JVM
+		 * startup, and also the average over N runs after that first run.
+		 */
+		final long nanosFirstTransfoStart = System.nanoTime();
+		sqlStmString = runOCL2PSQL(c);
+		printMetric(c, METRIC_OCL2PSQL_TIME_WARMUP, System.nanoTime() - nanosFirstTransfoStart);
 
-		try {
-			final File oclXMIFile = c.getOCLQueryXMIFile();
-			final OCL2SQL ocl2psql = new OCL2SQL(oclXMIFile);
-			final long nanosOCL2PSQLTransformationStart = System.nanoTime();
-			
-			sqlModel = ocl2psql.run();
-			sqlStmXMI = (SelectStatement) sqlModel.getResource().getContents().get(0);
-			final long nanosOCL2PSQLTransformationEnd = System.nanoTime();
-			transformationTimeNano = nanosOCL2PSQLTransformationEnd - nanosOCL2PSQLTransformationStart;
-		} catch (Exception e) {
-			e.printStackTrace();
-			transformationTimeNano = -1;
-		} finally {
-			printMetric(c, METRIC_OCL2PSQL_TIME, transformationTimeNano);
+		final long nanosTransfoStart = System.nanoTime();
+		for (int i = 0; i < N_RUNS; i++) {
+			sqlStmString = runOCL2PSQL(c);
 		}
+		printMetric(c, METRIC_OCL2PSQL_TIME, (System.nanoTime() - nanosTransfoStart)/N_RUNS);
 
-		if (sqlStmXMI != null) {
-			String sqlStmString;
+		if (sqlStmString != null) {
 			try {
 				boolean totalCorrect = true;
 				long totalTestTime = 0L;
-				
-				sqlStmString = new SQL2Text(sqlModel.getResource()).run();
 
 				//	Correctness and execution time of the generated query
 				for (int iScenario = 1; iScenario <= scenarios; iScenario++) {
@@ -57,10 +58,10 @@ public class Solution {
 					final long nanosTestSQLEnd = System.nanoTime();
 					totalTestTime += nanosTestSQLEnd - nanosTestSQLStart;
 					totalCorrect = totalCorrect && localCorrect;
-//					final String result = localCorrect?"passed":"failed";
-//					printMetric(c, String.format(METRIC_SCENARIO_PREFIX, iScenario), result);
+					final String result = localCorrect ? "passed" : "failed";
+					printMetric(c, String.format(METRIC_SCENARIO_PREFIX, iScenario), result);
 				}
-				final String result = totalCorrect?"passed":"failed";
+				final String result = totalCorrect ? "passed" : "failed";
 				printMetric(c, METRIC_SCENARIO_ALL_PREFIX, result);
 				if (totalCorrect) {
 					printMetric(c, METRIC_TEST_TIME, totalTestTime);
@@ -68,6 +69,25 @@ public class Solution {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		}
+	}
+
+	private String runOCL2PSQL(Configuration c) {
+		try {
+			final File oclXMIFile = c.getOCLQueryXMIFile();
+			final OCL2SQL ocl2psql = new OCL2SQL(oclXMIFile);
+
+			// Run the M2M transformation for OCL2PSQL
+			EmfModel sqlModel = ocl2psql.run();
+
+			/*
+			 * Reviewer 2 suggested including M2T in measurement to mimic the extra M2M
+			 * transformation in the reference implementation.
+			 */
+			return new SQL2Text(sqlModel.getResource()).run();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 
